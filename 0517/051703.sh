51c683edd58e684d2f6357c3dd278b8cb387dc17
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2018-05-17 22:29:43 +0000

    5/17の課題

---
+++ 0517/051703.sh
@@ -0,0 +1,6 @@
+mkdir -p dir1/dir2/dir3
+cd dir1
+cd dir2
+cd dir3
+rm file{01..10}
+touch file01 file02 file03 file04 file05 file06 file07 file08 file09 file10
---
+++ 0517/051704.sh
@@ -0,0 +1,6 @@
+mkdir -p dir1/dir2/dir3
+cd dir1
+cd dir2
+cd dir3
+touch file01 file02 file03 file04 file05 file06 file07 file08 file09 file10
+rm file01 file02 file03 file04 file05
