205556b54ed5b089926d5abc6f8bf6733eb04606
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2018-06-28 03:57:45 +0000

    6/28

--- 0628/062802.sh
+++ 0628/062802.sh
@@ -5,12 +5,4 @@ if [ "$#" -eq 0 ]; then
 	exit 1
 fi
 
-if [ -z "$directory" ]; then
-	directory='.'
-fi
-
-if [ -z "$name" ]; then
-	name='*'
-fi
-
 find "$1" -type f | xargs du -b | sort -n | tac | head -n 5
