09ebb38c4bac878be6d15dea03ec53ca44135784
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2018-07-03 00:54:12 +0000

    0614

--- 0619/061901.sh
+++ 0619/061901.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-while [ 1 -eq 1 ];
-do
-  echo $((RANDOM%110))>> age
+kazu=$(sed p age | sort -n |uniq -c |awk '{print $2}')
+kosuu=$(sed p age | sort -n |uniq -c |awk '{print $1}')
+sel=0
+i=0
+sta=0
+fini=9
+ kei ()
+ {
+sel=$((sel + $1))
+if [ $i -eq $fini ]; then
+						                 	echo "$sta~$fini $sel" 
+										         	sta=$((sta + 10))
+															fini=$((fini + 10))
+															sel=0
+fi
+}
+for a in $kosuu
+do 	
+															kei $a
+															i=$((i + 1))
 done
